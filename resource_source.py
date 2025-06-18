a4f1558ba35278c6c35c520cef18af85e472dc73
SameerNauman <s.nauman2020@gmail.com>
2025-06-18 00:33:58 -0700

    Update resource_source.py

--- resource_source.py
+++ resource_source.py
@@ -14,22 +14,27 @@ class ResourceSource:
     def is_depleted(self):
         return self.amount <= 0
 
-    def gather(self, amount_requested):
-        gathered = min(self.amount, amount_requested)
-        self.amount -= gathered
-        return gathered
-    
-    def draw(self, screen, OFFSET_X, OFFSET_Y, CAMERA_X, CAMERA_Y, TILE_WIDTH, TILE_HEIGHT, resources):
+    def draw(self, screen, OFFSET_X, OFFSET_Y, CAMERA_X, CAMERA_Y, TILE_WIDTH, TILE_HEIGHT, SCREEN_WIDTH, SCREEN_HEIGHT, resources, VISIBILITY_MAP=None):
+        # Only draw if tile is visible (default: fully visible)
+        visible = True
+        if VISIBILITY_MAP is not None:
+            if 0 <= self.x < len(VISIBILITY_MAP) and 0 <= self.y < len(VISIBILITY_MAP[0]):
+                visible = VISIBILITY_MAP[self.x][self.y] == 2
+            else:
+                visible = False
+        if not visible:
+            return
+
         draw_x = self.x + OFFSET_X
         draw_y = self.y + OFFSET_Y
         screen_x = (draw_x - draw_y) * TILE_WIDTH // 2 + (SCREEN_WIDTH // 2) + CAMERA_X
         screen_y = (draw_x + draw_y) * TILE_HEIGHT // 2 + (SCREEN_HEIGHT // 4) + CAMERA_Y
         screen_y += TILE_HEIGHT // 2
 
-        for resource in resources:
-            if self.resource_type == "food":
-                pygame.draw.circle(screen, (128, 0, 128), (int(screen_x), int(screen_y)), 10)
-            elif self.resource_type == "gold":
-                pygame.draw.circle(screen, (255, 165, 0), (int(screen_x), int(screen_y)), 10)
-            else:
-                pygame.draw.circle(screen, (0, 100, 0), (int(screen_x), int(screen_y)), 10)
+        # Draw this resource only once, not for every resource in the list
+        if self.resource_type == "food":
+            pygame.draw.circle(screen, (128, 0, 128), (int(screen_x), int(screen_y)), 10)
+        elif self.resource_type == "gold":
+            pygame.draw.circle(screen, (255, 165, 0), (int(screen_x), int(screen_y)), 10)
+        else:
+            pygame.draw.circle(screen, (0, 100, 0), (int(screen_x), int(screen_y)), 10)
